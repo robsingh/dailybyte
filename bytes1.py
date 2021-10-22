9f4a86273c849b8733a771e0f33e564c0820efb5
Robin Deonia <23559745+robsingh@users.noreply.github.com>
2021-10-21 17:56:25 -0700

    Delete bytes1.py

--- bytes1.py
+++
@@ -1,8 +0,0 @@
-"""
-Given an integer array, nums, return True if any value within 'nums'
-its double also exists within the array. 
-Example: 
-nums = [4,3,9,8] return True because 4 and 8 both appear in the array.
-nums = [9,2,3,5] return False because double of none of the elements appear in the array.
-
-"""
