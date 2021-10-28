2cee30f8381d65f02f8bdf033122cc42239057c8
robsingh <robie10895@gmail.com>
2021-10-27 22:16:12 -0700

    Brute Force solution

--- prob1.py
+++ prob1.py
@@ -4,4 +4,20 @@ The function must return True if any value appears at least twice in
 the array, and must return False if every element is distinct.
 """
 
+"""Brute Force approach O(N^2)"""
+
+
+
+class Solution:
+    def check_Duplicate(self,num):
+        for i in range(len(num)):
+            for j in range(0,i):
+                if num[i] == num[j]:
+                    return True
+        return False
+
+
+check = Solution()
+print(check.check_Duplicate([1,2,4,4]))
+
 
