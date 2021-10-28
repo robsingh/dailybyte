28a4e2cfdbed740505a70aeb2ed3d91ae5c30cfa
robsingh <robie10895@gmail.com>
2021-10-27 21:52:19 -0700

    Adding New Problem Statement

---
+++ prob1.py
@@ -0,0 +1,7 @@
+"""
+Given an array of integers, find if the array contains duplicates. 
+The function must return True if any value appears at least twice in 
+the array, and must return False if every element is distinct.
+"""
+
+
