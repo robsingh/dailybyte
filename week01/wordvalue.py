15c646616a0205657d4e8212c5b5f06c5ff4e2f8
robsingh <robie10895@gmail.com>
2023-05-17 17:36:50 -0700

    Initial Commit

---
+++ week01/wordvalue.py
@@ -0,0 +1,25 @@
+"""
+This week you will write a script to calculate the (language) dictionary word that would have the most value in Scrabble.
+
+Read in dictionary.txt (a copy of /usr/share/dict/words on my Mac) and 
+calculate the word that has the most value in Scrabble based on LETTER_SCORES which is imported in wordvalue-template.py.
+"""
+
+from data import DICTIONARY, LETTER_SCORES
+def load_words():
+    """Load dictionary into a list and return list."""
+    pass
+
+def calc_word_value():
+    """Calculate the value of the word entered into function using imported
+    constant mapping LETTER_SCORES"""
+    pass
+
+def max_word_value():
+    """Calculate the word with the max value, can receive a list of words as arg,
+    if none provided uses default dictionary"""
+    pass
+
+
+if __name__ == "__main__":
+    pass #run unittests to validate
