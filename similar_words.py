e7ba97a0ad82732c7755b9df7b547756ffd3e46a
robsingh <robie10895@gmail.com>
2024-03-12 11:11:46 -0400

    new approach

--- similar_words.py
+++ similar_words.py
@@ -11,17 +11,33 @@ Return a list of similar tag pairs (tuples):
  ...]
 """
 from difflib import SequenceMatcher
+from collections import defaultdict
 
 def similar_tags(tags):
     # brute force approach
+    # similar_pairs = []
+    # for i in range(len(tags)):
+    #     for j in range(i+1, len(tags)):
+    #         similarity = SequenceMatcher(None, tags[i], tags[j]).ratio()
+    #         if similarity > 0.95:
+    #             similar_pairs.append((tags[i], tags[j]))
+    # return similar_pairs
+
+    #another approach
+    grouped_tags = defaultdict(list)
+    for tag in tags:
+        normalized_tag = tag.replace(" ", "").lower()
+        grouped_tags[normalized_tag].append(tag)
+        
     similar_pairs = []
-    for i in range(len(tags)):
-        for j in range(i+1, len(tags)):
-            similarity = SequenceMatcher(None, tags[i], tags[j]).ratio()
-            if similarity > 0.95:
-                similar_pairs.append((tags[i], tags[j]))
+    for group in grouped_tags.values():
+        for i in range(len(group)):
+            for j in range(i+1, len(group)):
+                similarity = SequenceMatcher(None, group[i], group[j]).ratio()
+                if similarity > 0.95:
+                    similar_pairs.append((group[i], group[j]))
     return similar_pairs
 
-
+   
 tags = ['cheat sheets', 'cheat sheet', 'webscraping', 'web scraping', 'contextmanagers', 'contextmanager']
 print(similar_tags(tags))
