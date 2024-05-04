8caf1056e7adc2b3e62b4c220119d25fb132ee31
robsingh <robie10895@gmail.com>
2024-05-04 13:11:54 -0400

    adding a terminating statement

--- byte05.py
+++ byte05.py
@@ -2,18 +2,17 @@
 Write a generator that returns every 100th day counting forward from the PYBITES_BORN date.
 """
 from datetime import datetime, timedelta
-from gendates import gen_special_pybites_dates
+# from gendates import gen_special_pybites_dates
 
 
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 def gen_special_pybites_dates():
     day = PYBITES_BORN
-    while True:
+    end_date = PYBITES_BORN + timedelta(days=365*1) #stop after one year
+    while day <= end_date:
         yield day
         day += timedelta(days=100)
-
-
 
 for date in gen_special_pybites_dates():
     print(date)
