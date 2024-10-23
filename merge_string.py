399a604fe7d04169480c79467822ab457f1b14a3
robsingh <robie10895@gmail.com>
2024-10-23 12:23:28 -0400

    merge strings

--- merge_string.py
+++ merge_string.py
@@ -1,7 +1,31 @@
 '''
 Given two strings, s and t, merge the two strings together alternating characters starting with s.
-Note: If one string is longer than the other, append the remaining characters of the longer string at the end of the merged string.
+Note: If one string is longer than the other, append the remaining characters of the longer string 
+at the end of the merged string.
 
 Example:
-s = "abc", t = "def", return "adbecf".
+s = "abc", t = "def", return "adbecf"
 '''
+
+
+def merge_strings(s:str,t:str) -> str:
+    result = []
+    i, j = 0,0
+
+    # Loop through both strings and alternate characters
+    while i < len(s) and j < len(t):
+        result.append(s[i])
+        result.append(t[j])
+        i += 1
+        j += 1
+
+    # Append the remaining characters from the longer string
+    result.append(s[i:])
+    result.append(t[j:])
+
+    return ''.join(result)
+
+
+s = "abc"
+t = "def"
+print(merge_strings(s,t))
